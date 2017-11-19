38a36331ab4f2b4306f3f9d591fe322b132ee408
aurorex <selva_beat@hotmail.com>
2017-11-19 12:00:11 +0100

    agregando funcionalidad en el button,y agregando funcion countCaracters

--- js/app.js
+++ js/app.js
@@ -1,29 +1,51 @@
 window.addEventListener('load', function() {
   var textarea = document.getElementById('textarea');
   var button = document.getElementById('button');
-  var flag = false;
-  textarea.addEventListener('keypress', activeButton);
-
+// evento para validar la entrada del texto
+  textarea.addEventListener('keydown', activeButton);
   function activeButton(event) {
-    flag = true;
+    button.classList.add('button');
     button.classList.add('color');
-    if (textarea.value.length < 1  ) {
-      console.log(textarea.value.length);
-      button.removeAttribute('disabled');
-      flag = false;
+    // lastelement=== 8,ejecute de todos modos
+    if (event.which === 8 || event.which === 32) {
       button.classList.remove('color');
+      button.classList.remove('button');
+      button.removeEventListener('click', saveMessage);
+    } else {
+      button.addEventListener('click', saveMessage);
+    };
+  };
+// evento para validar la cantidad de caracteres
+  textarea.addEventListener('keydown', countCaracters);
+  function countCaracters(event) {
+    var count = document.getElementById('count');
+    count.innerHTML = 140 - textarea.value.length;
+    if (event.which === 8) {
+      count.innerHTML = 140 - textarea.value.length;
     }
-
+    if (textarea.value.length > 140) {
+      count.innerHTML = - textarea.value.length + 140;
+      count.classList.add('color-red');
+      button.classList.remove('color');
+      button.classList.remove('button');
+      button.removeEventListener('click', saveMessage);
+      textarea.removeEventListener('keydown', activeButton);
+    } else {
+      count.classList.remove('color-red');
+      //button.classList.add('color');
+      button.addEventListener('click', saveMessage);
+      textarea.addEventListener('keydown', activeButton);
+    }
   };
 
+// evento para guardar el texto
   button.addEventListener('click', saveMessage);
 
   function saveMessage(event) {
-    textarea.value ;
     var pharraphe = document.createElement('p');
     var tweets = document.getElementById('tweets');
-    console.log(pharraphe);
     pharraphe.innerHTML = textarea.value;
     tweets.appendChild(pharraphe);
+    pharraphe.classList.add('pharraphe');
   };
 });
